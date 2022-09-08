9e397bf38ee4f5a59fdbae4507597e44e45a096e
Atanas Atanasov <scriptex.bg@gmail.com>
2022-09-08 10:32:02 +0300

    Add all files

--- abbreviate.js
+++ abbreviate.js
@@ -1,15 +1,15 @@
 // Code adapted from https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
 
-const SI_SYMBOL = ["", "k", "M", "B", "T", "P", "E"];
+const SI_SYMBOL = ['', 'k', 'M', 'B', 'T', 'P', 'E'];
 
-module.exports = (number) => {
-  const tier = (Math.log10(number) / 3) | 0;
+module.exports = number => {
+	const tier = (Math.log10(number) / 3) | 0;
 
-  if (tier == 0) return +Number(number).toFixed(2);
+	if (tier == 0) return +Number(number).toFixed(2);
 
-  const suffix = SI_SYMBOL[tier];
-  const scale = Math.pow(10, tier * 3);
-  const scaled = number / scale;
+	const suffix = SI_SYMBOL[tier];
+	const scale = Math.pow(10, tier * 3);
+	const scaled = number / scale;
 
-  return scaled.toFixed(1) + suffix;
+	return scaled.toFixed(1) + suffix;
 };
--- api/analytics.js
+++ api/analytics.js
@@ -1,38 +1,38 @@
-const fetchReport = require("../fetchReport.js");
-const abbreviate = require("../abbreviate.js");
+const fetchReport = require('../fetchReport.js');
+const abbreviate = require('../abbreviate.js');
 
 const key = {
-  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: process.env.CLIENT_EMAIL,
+	private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+	client_email: process.env.CLIENT_EMAIL
 };
 
 module.exports = async (req, res) => {
-  const {
-    query: {
-      viewId = "",
-      metric = "ga:users",
-      startDate = "7daysAgo",
-      endDate = "today",
-      unit = "/week",
-      title = "users",
-      color = "green",
-      style = "plastic",
-      si = "true",
-    },
-  } = req;
+	const {
+		query: {
+			viewId = '',
+			metric = 'ga:users',
+			startDate = '7daysAgo',
+			endDate = 'today',
+			unit = '/week',
+			title = 'users',
+			color = 'green',
+			style = 'plastic',
+			si = 'true'
+		}
+	} = req;
 
-  return fetchReport(key, viewId, metric, startDate, endDate)
-    .then(({ data }) => {
-      const value = data.reports[0].data.totals[0].values[0];
-      return res.redirect(
-        302,
-        `https://img.shields.io/badge/${encodeURIComponent(title)}-${
-          si == "true" ? abbreviate(value) : value
-        }${encodeURIComponent(unit)}-${color}.svg?style=${style}`
-      );
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.sendStatus(500);
-    });
+	return fetchReport(key, viewId, metric, startDate, endDate)
+		.then(({ data }) => {
+			const value = data.reports[0].data.totals[0].values[0];
+			return res.redirect(
+				302,
+				`https://img.shields.io/badge/${encodeURIComponent(title)}-${
+					si == 'true' ? abbreviate(value) : value
+				}${encodeURIComponent(unit)}-${color}.svg?style=${style}`
+			);
+		})
+		.catch(err => {
+			console.error(err);
+			return res.sendStatus(500);
+		});
 };
